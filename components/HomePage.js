76aead7a883e3ed751bf4e857ccaaff185c3ffc3
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-08 01:19:16 +0330

    add svg

--- components/HomePage.js
+++ components/HomePage.js
@@ -24,7 +24,7 @@ function HomePage() {
                   A platform for obtaining queries on court cases.
                 </p>
                 <div className="relative flex flex-col sm:flex-row sm:space-x-4">
-                  <Link href="/auth">
+                  {/* <Link href="/auth">
                     <a className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-700 sm:mb-0 hover:bg-indigo-800 sm:w-auto rounded-2xl">
                       Get Started Now
                       <svg
@@ -59,7 +59,97 @@ function HomePage() {
                         <polyline points="12 5 19 12 12 19"></polyline>
                       </svg>
                     </a>
-                  </Link>
+                  </Link> */}
+                  <div className="p-3 text-white bg-indigo-700 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8 "
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                  <path d="M5 8v-3a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5"></path>
+                  <circle cx="6" cy="14" r="3"></circle>
+                  <path d="M4.5 17l-1.5 5l3 -1.5l3 1.5l-1.5 -5"></path>
+                </svg>
+              </div>
+                  <div className="p-3 text-white bg-indigo-700 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8 "
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M8 9l3 3l-3 3"></path>
+                  <line x1="13" y1="15" x2="16" y2="15"></line>
+                  <rect x="3" y="4" width="18" height="16" rx="2"></rect>
+                </svg>
+              </div>
+                  <div className="p-3 text-white bg-indigo-700 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8 "
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <line x1="9.5" y1="11" x2="9.51" y2="11"></line>
+                  <line x1="14.5" y1="11" x2="14.51" y2="11"></line>
+                  <path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path>
+                  <path d="M7 5h1v-2h8v2h1a3 3 0 0 1 3 3v9a3 3 0 0 1 -3 3v1h-10v-1a3 3 0 0 1 -3 -3v-9a3 3 0 0 1 3 -3"></path>
+                </svg>
+              </div>
+                  <div className="p-3 text-white bg-indigo-700 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8 "
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <polyline points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3"></polyline>
+                  <line x1="12" y1="12" x2="20" y2="7.5"></line>
+                  <line x1="12" y1="12" x2="12" y2="21"></line>
+                  <line x1="12" y1="12" x2="4" y2="7.5"></line>
+                  <line x1="16" y1="5.25" x2="8" y2="9.75"></line>
+                </svg>
+              </div>
+                  <div className="p-3 text-white bg-indigo-700 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8 "
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <line x1="15" y1="5" x2="15" y2="7"></line>
+                  <line x1="15" y1="11" x2="15" y2="13"></line>
+                  <line x1="15" y1="17" x2="15" y2="19"></line>
+                  <path d="M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-3a2 2 0 0 0 0 -4v-3a2 2 0 0 1 2 -2"></path>
+                </svg>
+              </div>
                 </div>
               </div>
             </div>
@@ -77,7 +167,7 @@ function HomePage() {
           </div>
         </div>
       </section>
-
+{/* 
       <section className="py-16 bg-white">
         <div className="container max-w-6xl mx-auto">
           <h2 className="text-4xl font-bold tracking-tight text-center">
@@ -244,7 +334,7 @@ function HomePage() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       <div className="bg-gray-900">
         <footer className="flex flex-wrap items-center justify-between p-3 m-auto">
