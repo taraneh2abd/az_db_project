23e6b3a9d68761c0b0f856d85895abf373df0c90
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-14 18:22:46 +0330

    comment the bug part(syntax_highliter)

--- components/ButtonWithInput.js
+++ components/ButtonWithInput.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import { docco } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const ButtonWithInput = ({ buttonText, placeholders }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -70,9 +70,11 @@ const ButtonWithInput = ({ buttonText, placeholders }) => {
             </div>
 
             <div className="relative ml-4 w-[1000px] bg-gray-300 p-4 rounded-2xl overflow-auto max-h-[400px]">
-              <SyntaxHighlighter language="sql" style={docco}>
+              {/* <SyntaxHighlighter language="sql" style={docco}> */}
+              <perf>
                 {apiMessage}
-              </SyntaxHighlighter>
+              </perf>
+              {/* </SyntaxHighlighter> */}
                           <button
                 className="absolute top-4 right-4 px-4 py-2 text-white bg-indigo-700 hover:bg-indigo-800 rounded-lg"
                 onClick={() => setShowERD(true)}
